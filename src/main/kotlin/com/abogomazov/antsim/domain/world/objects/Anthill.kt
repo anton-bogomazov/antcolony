c22d3704b2aada8359a84fc69b44130a3ac892b5
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2026-01-04 12:26:46 +0100

    proper food storage logic

--- src/main/kotlin/com/abogomazov/antsim/domain/world/objects/Anthill.kt
+++ src/main/kotlin/com/abogomazov/antsim/domain/world/objects/Anthill.kt
@@ -4,7 +4,14 @@ import com.abogomazov.antsim.domain.grid.CubeCoordinate
 
 class Anthill(
     position: CubeCoordinate,
-    val foodStored: UInt,
+    foodStored: UInt,
 ) : ImmovableWorldObject(position) {
+    var foodStored: UInt = foodStored
+        private set
+
+    fun store(amount: UInt) {
+        foodStored += amount
+    }
+
     override fun toString(): String = "Anthill[$foodStored] at $hex"
 }
